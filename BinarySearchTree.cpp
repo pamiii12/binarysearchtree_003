2f41fd392a3bac7e9a492f6269aa89a9ab738205
pamiii12 <fahmiadityaa03@gmail.com>
2023-06-29 13:35:12 +0700

    obyek node

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -1,3 +1,13 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+class Node
+{
+public :
+	string info;
+	Node* leftchild;
+	Node* rightchild;
+
+	// Constructor for the node class
+};
