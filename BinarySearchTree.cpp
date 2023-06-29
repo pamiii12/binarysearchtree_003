f359fdffc9853f7f14fcb4737afd58a13c0e8466
pamiii12 <fahmiadityaa03@gmail.com>
2023-06-29 14:50:18 +0700

    if else

--- BinarySearchTree.cpp
+++ BinarySearchTree.cpp
@@ -51,5 +51,15 @@ public:
 		{
 			parent->leftchild = newNode; // Make the left child of the parent point to the new node.
 		}
+		else if (element > parent->info) /* If the value in the data field of the new node is greater 
+			than that of the parent*/
+		{
+			parent->rightchild = newNode; //Make the right child of the parent point to the new node.
+		}
+	}
+
+	void search(string element, Node*& parent, Node*& currentNode)
+	{
+
 	}
 };
